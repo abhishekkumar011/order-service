e78dc0c8b677bf664b5c19581e2d9ea0caf64856
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2026-07-17 00:08:14 +0530

    feat: add service method to retrieve all orders

--- internal/service/order_service.go
+++ internal/service/order_service.go
@@ -91,16 +91,57 @@ func (s *OrderService) updateItems(event models.Event) error {
 	return err
 }
 
+func (s *OrderService) GetOrders() ([]models.Order, error) {
+
+	cursor, err := database.OrderCollection.Find(
+		context.Background(),
+		bson.M{},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var orders []models.Order
+
+	if err := cursor.All(context.Background(), &orders); err != nil {
+		return nil, err
+	}
+
+	return orders, nil
+}
+
 /*
 	1. type OrderService struct{} Think of it like a class in TypeScript.
 	2. bson.M is just a MongoDB map. It's like a JavaScript object: It's used to build MongoDB queries and updates.
 	3. in the func (s *OrderService) updateItems(event models.Event)
-	   - (s *OrderService) This is called the receiver. 
+	   - (s *OrderService) This is called the receiver.
 	     - s is just a variable name, like this in JavaScript.
 		 - The * means pointer. menas Use the original OrderService, not a copy.
 	   - updateItems is a method that belongs to OrderService. It's similar to a class method in JavaScript/TypeScript.
 	   - (event models.event) This is the parameter.
 	     - variable name: event
 		 - Type: models.Event
-	   - error means the return type of function but if everything is succed then it returns nil means no error	 
+	   - error means the return type of function but if everything is succed then it returns nil means no error
+
+	4. ([]models.Order, error) - Returns two Values 1. list of orders 2. error
+	    - bson.M{} inside M we can define filter based on that we can fetch documents
+		  - but {} means no filter it tells MongoDB Give me every document in this collection.  
+	
+	5. cursor, err := database.OrderCollection.Find(
+		context.Background(),
+		bson.M{},
+	)
+		- Think of a cursor as a bookmark or iterator.
+		- When you run this func you will get cursor instead of orders[] 
+		- Find() in go does not return the actual orders it returns a cursor.
+		  - cursor - A pointer that lets you read the matching documents.
+		  	- Go gives you something like this:
+			- Cursor
+               ↓
+              Currently pointing at Page 1
+			  - The cursor knows where the results are, but it hasn't copied them into Go memory yet.
+
+		- Cusort.All() - Because now we want to read everything the cursor found. Convert cursor into an array of orders	  
+		               - This means: Cursor read all the documents you're pointing to and copy them into this slice(order array).
 */
